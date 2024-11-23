ba97f7b7ca392d74333d8b409f368418ee3a65fa
Zhuo Zhang <imzhuo@foxmail.com>
2024-11-23 15:32:08 +0800

    feat: Implement debug postfix handling in CMake and add corresponding tests

--- test.py
+++ test.py
@@ -74,7 +74,7 @@ class RocBuildTest(unittest.TestCase):
                 self.assertTrue(os.path.exists('build/artifacts_path/subhello.exe'))
                 shutil.rmtree('build/artifacts_path')
 
-            self.check_generate('artifacts_path', args='-G "Visual Studio 17 2022" -A x64')
+            self.check_generate('artifacts_path')
             self.check_build('artifacts_path', '--config Release')
             self.assertTrue(os.path.exists('build/artifacts_path/Release/foo_static.lib'))
             self.assertTrue(os.path.exists('build/artifacts_path/Release/foo_shared.dll'))
@@ -102,6 +102,57 @@ class RocBuildTest(unittest.TestCase):
             self.assertTrue(os.path.exists('build/artifacts_path/subhello'))
 
         shutil.rmtree('build/artifacts_path')
+
+    def test_debug_postfix(self):
+        if os_name == 'windows':
+            # msbuild
+            self.check_generate('debug_postfix')
+            self.check_build('debug_postfix', '--config Debug')
+            self.assertTrue(os.path.exists('build/debug_postfix/Debug/foo_d.lib'))
+            self.assertTrue(os.path.exists('build/debug_postfix/Debug/hello_d.exe'))
+            shutil.rmtree('build/debug_postfix')
+
+            ret, out = check_output('cl')
+            if ret == 0:
+                # ninja
+                self.check_generate('debug_postfix', args='-G Ninja')
+                self.check_build('debug_postfix')
+                self.assertTrue(os.path.exists('build/debug_postfix/foo.lib'))
+                self.assertTrue(os.path.exists('build/debug_postfix/hello.exe'))
+                shutil.rmtree('build/debug_postfix')
+
+                self.check_generate('debug_postfix', args='-G Ninja -DCMAKE_BUILD_TYPE=Debug')
+                self.check_build('debug_postfix')
+                self.assertTrue(os.path.exists('build/debug_postfix/foo.lib'))
+                self.assertTrue(os.path.exists('build/debug_postfix/hello.exe'))
+                shutil.rmtree('build/debug_postfix')
+
+                # Ninja Multi-Config
+                self.check_generate('debug_postfix', args='-G "Ninja Multi-Config"')
+                self.check_build('debug_postfix', '--config Debug')
+                self.assertTrue(os.path.exists('build/debug_postfix/Debug/foo_d.lib'))
+                self.assertTrue(os.path.exists('build/debug_postfix/Debug/hello_d.exe'))
+                shutil.rmtree('build/debug_postfix')
+        else:
+            # Ninja Multi-Config
+            self.check_generate('debug_postfix', args='-G "Ninja Multi-Config"')
+            self.check_build('debug_postfix')
+            self.assertTrue(os.path.exists('build/debug_postfix/Debug/libfoo_d.a'))
+            self.assertTrue(os.path.exists('build/debug_postfix/Debug/hello_d'))
+            shutil.rmtree('build/debug_postfix')
+
+            # make
+            self.check_generate('debug_postfix')
+            self.check_build('debug_postfix')
+            self.assertTrue(os.path.exists('build/debug_postfix/libfoo.a'))
+            self.assertTrue(os.path.exists('build/debug_postfix/hello'))
+            shutil.rmtree('build/debug_postfix')
+
+            self.check_generate('debug_postfix', args='-DCMAKE_BUILD_TYPE=Debug')
+            self.check_build('debug_postfix')
+            self.assertTrue(os.path.exists('build/debug_postfix/libfoo.a'))
+            self.assertTrue(os.path.exists('build/debug_postfix/hello'))
+            shutil.rmtree('build/debug_postfix')
 
 
 if __name__ == "__main__":
