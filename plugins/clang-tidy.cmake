5b25575b5dcdbc0b9066ac33282ceac098fef8d3
Zhuo Zhang <imzhuo@foxmail.com>
2025-03-09 15:13:28 +0800

    refactor: Support apply MSVC builtin clang-tidy.exe
    https://discourse.cmake.org/t/add-custom-command-problem-with-quotes-and-spaces/11014/2

--- plugins/clang-tidy.cmake
+++ plugins/clang-tidy.cmake
@@ -1,35 +1,37 @@
 # Author: Zhuo Zhang <imzhuo@foxmail.com>
 # Homepage: https://github.com/zchrissirhcz/rocbuild
-# Last update: 2024-06-12 00:00:00
+# Last update: 2025-03-09 15:08:06
 
 # This cmake plugin let you run clang-tidy on specified target
 # Each time you run "make", it report warnings if potential bug found
 #
 # Usage:
 # include(clang-tidy.cmake)
-# cvpkg_apply_clang_tidy(${your_target})
+# rocbuild_apply_clang_tidy(clang-tidy-executable target)
+#
+# e.g.
+# # VS2022
+# rocbuild_apply_clang_tidy("C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang-tidy.exe" main)
+# 
+# # ubuntu 22.04
+# rocbuild_apply_clang_tidy(clang-tidy-18 main)
+#
 
 cmake_minimum_required(VERSION 3.15)
 include_guard()
 
-if(ANDROID AND (CMAKE_SYSTEM_NAME MATCHES "Windows"))
-  set(CLANG_TIDY_EXE "clang-tidy")  # NDK's clang-tidy.exe doesn't work on Windows
-else()
-  find_program(CLANG_TIDY_EXE NAMES "clang-tidy" REQUIRED)
-endif()
 
-if(CLANG_TIDY_EXE)
-  message(STATUS "CLANG_TIDY_EXE is: ${CLANG_TIDY_EXE}")
-else()
-  message(FATAL_ERROR "Failed to find clang-tidy executable file")
-  # "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang-tidy.exe"
-  # "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.0/LLVM-16.0.0-win64.exe"
-endif()
-
-function(cvpkg_apply_clang_tidy targetName)
+# Apply clang-tidy on source files of given target
+# CLANG_TIDY_EXECUTABLE: clang-tidy executable file path
+#   - Ubuntu: https://apt.llvm.org/
+#   - Windows: "https://github.com/llvm/llvm-project/releases/"
+#   - Windows with VS2022: "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang-tidy.exe"
+#   - Android NDK on Windows: don't use NDK's clang-tidy.exe. Use "clang-tidy" instead.
+# TARGET: target name
+function(rocbuild_apply_clang_tidy CLANG_TIDY_EXECUTABLE TARGET)
   # collecting absolute paths for each source file in the given target
-  get_target_property(target_sources ${targetName} SOURCES)
-  get_target_property(target_source_dir ${targetName} SOURCE_DIR)
+  get_target_property(target_sources ${TARGET} SOURCES)
+  get_target_property(target_source_dir ${TARGET} SOURCE_DIR)
   # message(STATUS "target_source_dir: ${target_source_dir}")
   # message(STATUS "target_sources:")
   set(src_path_lst "")
@@ -44,15 +46,14 @@ function(cvpkg_apply_clang_tidy targetName)
   endforeach()
 
   # prepare clang-tidy command with arguments
-  set(clang_tidy_raw_command "${CLANG_TIDY_EXE} -p ${CMAKE_BINARY_DIR} ${src_path_lst}")
-  
-  # cmake only accept semicolon(;) separated list when a command with arguments is given
-  string(REPLACE " " ";" clang_tidy_converted_command "${clang_tidy_raw_command}")
-  # message(STATUS "clang_tidy_raw_command: ${clang_tidy_raw_command}")
-  # message(STATUS "clang_tidy_converted_command: ${clang_tidy_converted_command}")
+  set(clang_tidy_full_command "${CLANG_TIDY_EXECUTABLE}")
+  list(APPEND clang_tidy_full_command "-p")
+  list(APPEND clang_tidy_full_command "${CMAKE_BINARY_DIR}")
+  list(APPEND clang_tidy_full_command "${src_path_lst}")
+
   add_custom_target(
-    clang-tidy
-    COMMAND ${clang_tidy_converted_command}
+    ${TARGET}_clang-tidy
+    COMMAND ${clang_tidy_full_command}
   )
-  add_dependencies(${targetName} clang-tidy)
+  add_dependencies(${TARGET} ${TARGET}_clang-tidy)
 endfunction()
