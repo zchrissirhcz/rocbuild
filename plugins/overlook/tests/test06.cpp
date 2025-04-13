d4998ecd42883a5900b56bd5541e0c5c2a97aa23
Zhuo Zhang <imzhuo@foxmail.com>
2025-04-13 10:35:45 +0800

    [feat] overlook.cmake add dangling reference case for rule6

--- plugins/overlook/tests/test06.cpp
+++ plugins/overlook/tests/test06.cpp
@@ -6,10 +6,39 @@ int* get_lucky_number()
     return &data;
 }
 
-int main()
+int return_stack_addr()
 {
     int* lucky_number = get_lucky_number();
     printf("lucky number is %d\n", *lucky_number);
+
+    return 0;
+}
+
+
+#include <string>
+#include <iostream>
+
+std::string& f()
+{
+    std::string s = "Example";
+    return s; // exits the scope of s:
+              // its destructor is called and its storage deallocated
+}
+
+int dangling_reference()
+{ 
+    std::string& r = f(); // dangling reference
+    std::cout << r;       // undefined behavior: reads from a dangling reference
+    std::string s = f();  // undefined behavior: copy-initializes from a dangling reference
+    std::cout << s << std::endl;
+    return 0;
+}
+
+
+int main(int argc, char** /*argv*/)
+{
+    return_stack_addr();
+    dangling_reference();
 
     return 0;
 }
