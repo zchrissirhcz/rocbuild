e39507b1db2e5ff72773368ba8b41ed3dafef14d
Zhuo Zhang <imzhuo@foxmail.com>
2025-06-17 23:16:35 +0800

    Generate libs and bins in root build dir

--- basic.cmake
+++ basic.cmake
@@ -12,3 +12,27 @@ set(CMAKE_DEBUG_POSTFIX "_d")
 
 # Enable fPIC
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
+################################################################################
+# Generate lib(.a/.lib/.so/.dll/...) and executable files in the same directory
+################################################################################
+if(NOT CMAKE_RUNTIME_OUTPUT_DIRECTORY)
+  # Where add_executable() generates executable file
+  # Where add_library(SHARED) generates .dll file on Windowos
+  set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}" CACHE INTERNAL "")
+endif()
+
+if(NOT CMAKE_LIBRARY_OUTPUT_DIRECTORY)
+  # Where add_library(MODULE) generates loadable module file (.dll or .so)
+  # Where add_library(SHARED) generates shared library (.so, .dylib)
+  set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}" CACHE INTERNAL "")
+endif()
+
+if(NOT CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
+  # Where add_library(STATIC) generates static library file
+  # Where add_library(SHARED) generates the import library file (.lib) of the shared library (.dll) if exports at least one symbol
+  # Where add_executable() generates the import library file (.lib) of the executable target if ENABLE_EXPORTS target property is set
+  # Where add_executable() generates the linker import file (.imp on AIX) of the executable target if ENABLE_EXPORTS target property is set
+  # Where add_library(SHARED) generates the linker import file (.tbd) of the shared library target if ENABLE_EXPORTS target property is set
+  set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}" CACHE INTERNAL "")
+endif()
--- rocbuild.cmake
+++ rocbuild.cmake
@@ -9,31 +9,6 @@ cmake_minimum_required(VERSION 3.13)
 
 include_guard()
 
-
-macro(rocbuild_set_artifacts_path)
-  if(NOT CMAKE_RUNTIME_OUTPUT_DIRECTORY)
-    # Where add_executable() generates executable file
-    # Where add_library(SHARED) generates .dll file on Windowos
-    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}" CACHE INTERNAL "")
-  endif()
-
-  if(NOT CMAKE_LIBRARY_OUTPUT_DIRECTORY)
-    # Where add_library(MODULE) generates loadable module file (.dll or .so)
-    # Where add_library(SHARED) generates shared library (.so, .dylib)
-    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}" CACHE INTERNAL "")
-  endif()
-
-  if(NOT CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
-    # Where add_library(STATIC) generates static library file
-    # Where add_library(SHARED) generates the import library file (.lib) of the shared library (.dll) if exports at least one symbol
-    # Where add_executable() generates the import library file (.lib) of the executable target if ENABLE_EXPORTS target property is set
-    # Where add_executable() generates the linker import file (.imp on AIX) of the executable target if ENABLE_EXPORTS target property is set
-    # Where add_library(SHARED) generates the linker import file (.tbd) of the shared library target if ENABLE_EXPORTS target property is set
-    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}" CACHE INTERNAL "")
-  endif()
-endmacro()
-
-
 macro(rocbuild_enable_ninja_colorful_output)  
   # When building a CMake-based project, Ninja may speedup the building speed, comparing to Make.
   # However, with `-GNinja` specified, compile errors are with no obvious colors.
