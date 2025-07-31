fae02bafafc13a4d89d9aad6801ed4e9bb97c132
Zhuo Zhang <imzhuo@foxmail.com>
2025-08-01 00:38:30 +0800

    refactor: use ZZBUILD_ROOT variable

--- toolchains/gcc-aarch64-none-linux-gnu.cmake
+++ toolchains/gcc-aarch64-none-linux-gnu.cmake
@@ -1,11 +1,11 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(TCDIR /opt/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu)
-set(CMAKE_C_COMPILER ${TCDIR}/bin/aarch64-none-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER ${TCDIR}/bin/aarch64-none-linux-gnu-g++)
+set(ZZBUILD_ROOT /opt/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu)
+set(CMAKE_C_COMPILER ${ZZBUILD_ROOT}/bin/aarch64-none-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER ${ZZBUILD_ROOT}/bin/aarch64-none-linux-gnu-g++)
 
-set(CMAKE_FIND_ROOT_PATH /opt/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu)
+set(CMAKE_FIND_ROOT_PATH ${ZZBUILD_ROOT})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
 
 include(${CMAKE_CURRENT_LIST_DIR}/toolchain-checker.cmake)
